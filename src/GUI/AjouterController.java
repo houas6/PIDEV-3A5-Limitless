3a244f3638eb344d411fb029288841d3c04b4768
jaidaneachraf <80226456+jaidaneachraf@users.noreply.github.com>
2023-02-18 00:13:40 +0100

    first push

---
+++ src/GUI/AjouterController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author achra
+ */
+public class AjouterController implements Initializable {
+
+    @FXML
+    private TextField fixednom;
+    @FXML
+    private TextField fixedprenom;
+    @FXML
+    private Button fixajout;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+   // private void addperson(ActionEvent event) {
+      //  String nom=fxnom.getText();
+      //  String prenom=fxnom.getText();
+        
+        
+        
+    }
+    
+}
---
+++ src/greclamation/Greclamation.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package greclamation;
+
+import BD.MyConnection;
+import model.reclamations;
+import services.CrudReclamation;
+
+/**
+ *
+ * @author achra
+ */
+public class Greclamation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         MyConnection connexion = MyConnection.getInstance();
+         reclamations r = new reclamations("alaa", "carte mere","defaillante");
+         CrudReclamation crud = new CrudReclamation();
+         crud.ajouterreclamation(r);
+         //crud.modifierreclamation(r);
+         //crud.supprimerreclamation(0);
+         //System.out.println(crud.afficherreclamation());
+        // TODO code application logic here
+    }
+    
+}
