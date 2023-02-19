c8b8b05e4fc5cbfbcc9c067ede99bd717ef3dc0d
houas Med aziz <medaziz.houas@esprit.tn>
2023-02-19 15:35:11 +0100

    imgae done
    crud with controle de saisie and image done

--- Gestionproduit/src/edu/produit/gui/AjouterController.java
+++ Gestionproduit/src/edu/produit/gui/AjouterController.java
@@ -7,14 +7,18 @@ package edu.produit.gui;
 
 import edu.produit.entites.Produit;
 import edu.produit.services.CRUDProduit;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -26,6 +30,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -81,7 +86,7 @@ public class AjouterController implements Initializable {
     @FXML
     private Button chooseimage;
     @FXML
-    private TableColumn<Produit, Byte> fxximage1;
+    private TableColumn<Produit, byte[]> fxximage1;
     
     
     /*public void setID_produit(String message)
@@ -112,9 +117,31 @@ public class AjouterController implements Initializable {
         fxxprix.setCellValueFactory(new PropertyValueFactory<>("prix"));
         fxxdescription.setCellValueFactory(new PropertyValueFactory<>("description"));
          fxxiduser.setCellValueFactory(new PropertyValueFactory<>("id_user"));
-         fxximage1.setCellValueFactory(new PropertyValueFactory<>("image"));
-        
-             for (Produit Pe : cr.afficherproduit()) {
+       fxximage1.setCellFactory(column -> {
+    TableCell<Produit, byte[]> cell = new TableCell<Produit, byte[]>() {
+        private final ImageView imageView = new ImageView();
+
+        @Override
+        protected void updateItem(byte[] item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty || item == null) {
+                imageView.setImage(null);
+            } else {
+                Image image = new Image(new ByteArrayInputStream(item));
+                imageView.setImage(image);
+            }
+
+            setGraphic(imageView);
+        }
+    };
+
+    return cell;
+});
+       populateTable();
+         
+         
+         for (Produit Pe : cr.afficherproduit()) {
             table_produit.getItems().add(Pe);
         }
     }    
@@ -332,6 +359,33 @@ private void Ajouterproduit(ActionEvent event) {
         Image image = new Image(selectedFile.toURI().toString());
         fximage.setImage(image);
     }
+    }
+    
+    public void populateTable(){
+        CRUDProduit cr= new CRUDProduit();
+        
+        List<Produit> li =cr.afficherproduit();
+        ObservableList<Produit> data = FXCollections.observableArrayList(li);
+                  
+
+        
+          
+        fxxidproduit.setCellValueFactory(
+                new PropertyValueFactory<>("id_produit"));
+        fxxnomproduit.setCellValueFactory(
+                new PropertyValueFactory<>("nom_produit"));
+ 
+       
+        fxxprix.setCellValueFactory(
+                new PropertyValueFactory<>("prix"));
+       fxxdescription.setCellValueFactory(  
+                new PropertyValueFactory<>("description")) ; 
+        
+        fxxiduser.setCellValueFactory(
+                new PropertyValueFactory<>("id_user"));
+        fxximage1.setCellValueFactory(
+                new PropertyValueFactory<>("image"));
+        table_produit.setItems(data);
     }
 }
    
